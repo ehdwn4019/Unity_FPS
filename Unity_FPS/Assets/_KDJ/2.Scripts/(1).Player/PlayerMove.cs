26d272ae24a80eee1ebc0457cdf53f030349cb86
ehdwn4019 <ehdwn7531@gmail.com>
2020-06-15 15:38:59 +0900

    -바닥,벽,플레이어 생성 -카메라 1인칭,3인칭 구현 -플레이어 이동,회전 구현

---
+++ Unity_FPS/Assets/_KDJ/2.Scripts/(1).Player/PlayerMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed=5.0f; //이동속도 
+    CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //캐릭터컨트롤러 컴포넌트 가져오기
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+        //pos = (Vector3.forward * v) + (Vector3.right * h);
+        //this.transform.Translate(pos * speed * Time.deltaTime);
+    }
+
+    private void move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(h, 0, v);
+        dir.Normalize();
+        transform.Translate(dir * speed * Time.deltaTime);
+
+
+        //카메라가 보는 방향으로 이동해야 한다 
+        dir = Camera.main.transform.TransformDirection(dir);
+        //transform.Translate(dir * speed * Time.deltaTime);
+
+        //심각한 문제 : 하늘 날라다님,땅 뚫음, 충돌처리 안됨 
+        controller.Move(dir*speed*Time.deltaTime);
+
+
+
+    }
+}
---
+++ Unity_FPS/Assets/_KDJ/2.Scripts/(1).Player/PlayerRotate.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotate : MonoBehaviour
+{
+    //카메라를 마우스 움직이는 방향으로 회전하기 
+    public float speed = 150; //회전속도 (Timme.DeltaTime을 통해 1초에 150도 회전)
+    //회전각도를 직접 제어하자
+    float angleX;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //카메라 회전 
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        float h = Input.GetAxis("Mouse X");
+
+        angleX += h * speed * Time.deltaTime;
+       
+        transform.eulerAngles = new Vector3(0, angleX, 0);
+
+        //angleY = Mathf.Clamp(angleY, -60, 60);
+
+        //위에거랑 아래거랑 같은 방식 
+
+        //if (angle.x > 60) angle.x = 60;
+        //if (angle.x < -60) angle.x = -60;
+
+    }
+}
